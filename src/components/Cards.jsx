cf945e11ea2fff67f9e968aabebe11f5fb29c839
Alejo Holmann <alejoholmann99@gmail.com>
2023-03-07 19:37:46 -0300

    new rick_and_morty

---
+++ src/components/Cards.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  const { characters } = props;
+
+  return (
+    <div>
+      {characters.map(({ name, species, gender, image }) => {
+        return (
+          <Card name={name} species={species} gender={gender} image={image} />
+        );
+      })}
+    </div>
+  );
+}
