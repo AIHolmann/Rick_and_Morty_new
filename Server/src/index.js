cf993c702482d1709945cb5e216095a2acf4caa7
Alejo Holmann <alejoholmann99@gmail.com>
2023-04-22 17:01:04 -0300

    promises, server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,12 @@
 const http = require("http");
-const data = require("./utils/data");
+const { getCharById } = require("./controllers/getCharById");
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (req.url.includes("/rickandmorty/character")) {
       const id = req.url.split("/character/")[1];
-      const character = data.find((char) => char.id == id);
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(character));
+      getCharById(res, +id);
     }
   })
   .listen(3001, "localhost");
